8ffe4bdac958d737cee62cc1b821a09c9c1a8404
uzi <rytuiytyuttrye567@gmail.com>
2022-10-28 17:44:39 +0500

    add search input

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -8,8 +8,11 @@ function App() {
         <header className="page-header">
           <div className="wrapper">
             <div className="col-12">
-              <div className="page-header__banner">
+              <div className="page-header__banner mt-10px text-align-center">
                 <h1 className="page-title">Blogs </h1>
+              </div>
+              <div className="text-align-center">
+                <input type="text" className="search_nscc" placeholder="Search By Teams  ..."/>
               </div>
             </div>
           </div>
